5848f264f3caec1e25c6c690bc94ae0047f6a51f
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2022-12-27 19:53:21 +0800

    remove column color

--- lua/opts.lua
+++ lua/opts.lua
@@ -3,7 +3,6 @@ local opt = vim.opt
 local cmd = vim.api.nvim_command
 
 -- [[ Context ]]
-opt.colorcolumn = '120'          -- str:  Show col for max line length
 opt.number = true                -- bool: Show line numbers
 opt.relativenumber = true        -- bool: Show relative line numbers
 opt.scrolloff = 4                -- int:  Min num lines of context
