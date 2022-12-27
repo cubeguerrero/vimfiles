adaaf25eac597117d03aee0f68bd78598356bb74
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2022-12-27 19:57:25 +0800

    Add haskell-language-server

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -7,7 +7,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- [[ Init ]]
-local servers = { 'rust_analyzer', 'solargraph', 'tsserver' }
+local servers = { 'rust_analyzer', 'solargraph', 'tsserver', 'hls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
