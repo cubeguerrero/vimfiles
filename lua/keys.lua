63a462eca8a140ec9586657746fffcaac8653f82
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2023-01-10 09:46:44 +0800

    Add which-key registers

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,11 +1,30 @@
 -- [[ keys.lua ]]
 local map = vim.keymap.set
 local telescope = require('telescope.builtin')
+local wk = require('which-key')
 
 -- [[ Telescope ]]
 map('n', '<leader>ff', telescope.find_files, {})
 map('n', '<leader>fb', telescope.buffers, {})
 map('n', '<leader>fg', telescope.live_grep, {})
+map('n', '<leader>fr', telescope.oldfiles, {})
+map('n', '<leader>fn', [[:enew]], {})
+wk.register({
+  ["<leader>f"] = {
+    name = "+file",
+    f = { "<cmd>Telescope find_files<CR>", "Find File" },
+    b = { "<cmd>Telescope buffers<CR>", "Open Buffers" },
+    g = { "<cmd>Telescope live_grep", "Grep" },
+    n = { "<cmd>enew<cr>", "New File" },
+    r = { "<cmd>Telescope oldfiles<CR>", "Open Recent File" },
+  },
+})
 
 -- [[ NvimTreeToggle ]]
 map('n', '<leader>tt', [[:NvimTreeToggle<CR>]], {})
+wk.register({
+  ["<leader>t"] = {
+    name = "+tree",
+    t = { "<cmd>NvimTreeToggle<cr>", "Tree Toggle" },
+  },
+})
