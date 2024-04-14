15e62895e2440a44c14fadf08f8712a926610ce6
cubeguerrero <cuthbert.guerrero@gmail.com>
2024-04-14 20:56:38 +0800

    add java, json, yaml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,8 +17,11 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "plugins" },
   },
   defaults = {
