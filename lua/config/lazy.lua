0da1b6ebde2012ab9f7aab122792ef9e93a18318
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2025-06-30 21:27:09 +0800

    daily stuff

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,8 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.lang.elixir" },
+    -- { import = "lazyvim.plugins.extras.lang.elixir" },
+    { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
