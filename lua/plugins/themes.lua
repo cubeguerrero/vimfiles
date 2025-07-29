51b37f364d9d73199aed26f040bf160990a0183a
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2025-07-29 22:30:19 +0800

    some minor updates

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,9 +1,16 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-latte",
+      colorscheme = "catppuccin",
     },
   },
 }
